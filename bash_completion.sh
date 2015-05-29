abc1066bdbed01d07ee5b12bccff970c08e7a0fa
Yuri <yurikungfu@gmail.com>
2015-05-29 17:34:40 +0200

    Add some improvements on bash completion

--- bash_completion.sh
+++ bash_completion.sh
@@ -3,7 +3,7 @@
 # 29 May 2015 - Pradipta and Yuri
 _qpy() 
 {
-    local cur prev opts
+    local cur prev opts job_kind
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
@@ -21,13 +21,23 @@ _qpy()
 
     if [[ ${COMP_WORDS[@]} > 2 ]]
     then
-	prev_2="${COMP_WORDS[COMP_CWORD-2]}"
-	if [[ ${prev_2} == 'sub' ]] ; then
-            COMPREPLY=( $(compgen -f ${cur}) )
+	job_kind="${COMP_WORDS[1]}"
+	if [[ ${job_kind} == 'sub' ]] ; then
+            COMPREPLY=( $(compgen -o plusdirs -f ${cur}) )
+
+	    if [ ${#COMPREPLY[@]} -eq 1 ]; then
+		[ -d "$COMPREPLY" ] && LASTCHAR=/
+		COMPREPLY=$(printf %q%s "$COMPREPLY" "$LASTCHAR")
+	    else
+		for ((i=0; i < ${#COMPREPLY[@]}; i++)); do
+		    [ -d "${COMPREPLY[$i]}" ] && COMPREPLY[$i]=${COMPREPLY[$i]}/
+		done
+	    fi
+
             return 0
 	fi
     fi
 
 
 }
-complete -F _qpy qpy
+complete -o nospace -F _qpy qpy
