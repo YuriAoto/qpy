4891655435cc9908efa2410b90907d7eaad8c00c
Yuri Alexandre Aoto <yuri.aoto@theochem.uni-stuttgart.de>
2017-05-04 11:30:02 +0200

    Remove ulimit for memory

--- qpy-master.py
+++ qpy-master.py
@@ -408,7 +408,7 @@ class JOB():
         command += 'export QPY_NODE=' + str( self.node) + '; '
         command += 'export QPY_N_CORES=' + str( self.n_cores) + '; '
         command += 'export QPY_MEM=' + str( self.mem) + '; '
-        command += 'ulimit -Sv ' + str( self.mem*1048576) + '; '
+#        command += 'ulimit -Sv ' + str( self.mem*1048576) + '; '
         for sf in source_these_files:
            command += 'source ' + sf + '; '
         command += 'cd ' + self.info[1] + '; ' 
